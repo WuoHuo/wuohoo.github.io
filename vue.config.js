7585a16bd2c37dd3efeeb0ece321a395fd4729c4
wohuo <137469015@qq.com>
2022-03-19 15:18:25 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/wuohoo.github.io': '/'
-}
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production' ? '/wuohoo.github.io': '/'
+// }
